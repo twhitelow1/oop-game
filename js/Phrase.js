beaf18783b7d626d0f3e8cd9d637d6d0d65630a6
Todd Whitelow <todd@whitelow.tech>
2020-09-07 00:44:06 -0600

    test class

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,27 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+  constructor(phrase) {
+    this.phrase = phrase;
+  }
+  addPhraseToDisplay = () => {
+    const characters = this.phrase.split('');
+    phraseDiv = document.querySelector('#phrase ul')
+    console.log(phraseDiv);
+    // characters.each(character => {
+
+
+    // })
+  }
+  checkLetter = () => {
+
+  }
+
+  showMatchedLetter = () => {
+
+  }
+}
+
+const phrase = new Phrase("This is the phrase!");
