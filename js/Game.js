dc54eeee90701e0f4f222bc950acc8dc9b6f0bca
Todd Whitelow <todd@whitelow.tech>
2020-09-07 01:38:41 -0600

    add createPhrase method to game class

--- js/Game.js
+++ js/Game.js
@@ -8,4 +8,21 @@ class Game {
     this.phrases = phrases;
     this.activePhrase = activePhrase
   }
+  /**
+   * Create Phrases for use in game
+   * @return {array} An array of phrases that could be used in the game
+   */
+  createPhrases() {
+    const allPhrases = []
+    allPhrases.push(new Phrase("Part of the journey is the end"));
+    allPhrases.push(new Phrase("I would rather be a good man than a great king"));
+    allPhrases.push(new Phrase("Teach Me"));
+    allPhrases.push(new Phrase("Hulk smash"));
+    allPhrases.push(new Phrase("I can do this all day"));
+
+    return allPhrases
+  }
 }
+
+const game = new Game();
+gamePhrases = game.createPhrases();
