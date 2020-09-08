fb25e8ae690ca854be39594cde5dbe3d8ad900a6
Todd Whitelow <todd@whitelow.tech>
2020-09-08 16:16:31 -0600

    add keypress listener and functionality

--- js/app.js
+++ js/app.js
@@ -7,6 +7,14 @@ addEventListenerList = (list, event) => {
     list[i].addEventListener(event, event => game.handleInteraction(event.target));
   }
 }
+getKeyBoardElement = (eventKeyPressed, onScreenKeys) => {
+  for (let i = 0; i < onScreenKeys.length; i++) {
+    if (onScreenKeys[i].innerHTML === eventKeyPressed) {
+      return onScreenKeys[i]
+    }
+  }
+  return false
+}
 
 let game = {};
 const button = document.getElementById('btn__reset');
@@ -18,5 +26,14 @@ button.addEventListener('click', event => {
 })
 
 const keys = document.querySelectorAll('#qwerty button');
-
 addEventListenerList(keys, 'click');
+document.getElementById('btn__reset');
+
+window.addEventListener('keydown', event => {
+  const keyPressed = event.key
+  if (!getKeyBoardElement(keyPressed, keys)) {
+    return
+  } else {
+    game.handleInteraction(getKeyBoardElement(keyPressed, keys))
+  }
+});
